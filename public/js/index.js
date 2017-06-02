8fd866ef95c70014bf7101312cf961cc958ec426
serge-teretshenko <teretshenko.s@gmail.com>
2017-06-02 13:12:33 +0300

    Add jquery

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages-list').append(li);
+});
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'Fuck you!'
+}, function (data) {
+  console.log(data);
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
