edb6a79615a76f9ded3bccddca0434815ef58774
serge-teretshenko <teretshenko.s@gmail.com>
2017-06-02 10:58:52 +0300

    Emit new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,12 @@ let socket =  io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('New email', email);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -3,15 +3,13 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
 app.use(express.static(publicPath));
-
-
 
 io.on('connection', (socket) => {
   console.log('New user connected');
@@ -21,11 +19,11 @@ io.on('connection', (socket) => {
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: new Date.getTime()
-    })
+      createdAt: new Date().getTime()
+    });
   })
 
-  socket.on('disconnect', (socket) => {
+  socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
