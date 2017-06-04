e4c57d688d61c7799252b2ff58ae60e1bb98bea0
serge-teretshenko <teretshenko.s@gmail.com>
2017-06-04 20:45:41 +0300

    Add users.test

--- server/utils/users.js
+++ server/utils/users.js
@@ -4,16 +4,29 @@
   room: 'Gym'
 }]
 
-class Person {
-  constructor (name, age) {
-    this.name = name;
-    this.age = age;
+class Users {
+  constructor () {
+    this.users = [];
   }
-  getUserDescription () {
-    return `${this.name} is ${this.age} years old`;
+  addUser (id, name, room) {
+    let user = {id, name, room};
+    this.users.push(user);
+    return user;
   }
 }
 
-var me = new Person('Valery', 25);
-var description = me.getUserDescription();
-console.log(description);
+module.exports = {Users};
+
+// class Person {
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} years old`;
+//   }
+// }
+//
+// var me = new Person('Valery', 25);
+// var description = me.getUserDescription();
+// console.log(description);
